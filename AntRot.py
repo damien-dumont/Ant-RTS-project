cfee0ad0917372c5e3f1f522f92d2478b31760f5
damien-dumont <55583762+damien-dumont@users.noreply.github.com>
2019-10-11 16:26:16 +0200

    Destination added
    Rotation still needs a fix

--- AntRot.py
+++ AntRot.py
@@ -3,16 +3,24 @@ from pygame.locals import *
 
 class Hero(pygame.sprite.Sprite):
 	
-	def __init__(self, x, y,DIRECTION,upKeyPressed,downKeyPressed,leftKeyPressed,rightKeyPressed, leftMousePressed, rightMousePressed, oneKeyPressed, HP, game):
+	def __init__(self, x, y,DIRECTION,upKeyPressed,downKeyPressed,leftKeyPressed,rightKeyPressed, leftMousePressed, rightMousePressed, oneKeyPressed, HP, game, pos=(0, 0), size=(150, 150)):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = self.perso_rotated_surf = pygame.image.load("Sprites/Ant-rot.png").convert_alpha()
+		self.image = pygame.image.load("Sprites/Ant-mid.png").convert_alpha()
+		self.original_image = pygame.image.load("Sprites/Ant-mid.png").convert_alpha()
+		self.original_image = pygame.Surface(size)
+		pygame.draw.line(self.original_image, (255, 0, 255), (size[0] / 2, 0), (size[0] / 2, size[1]), 3)
+		pygame.draw.line(self.original_image, (0, 255, 255), (size[1], 0), (0, size[1]), 3)
+		self.image = self.original_image
+		self.rect = self.image.get_rect()
+		self.rect.center = pos
+		self.angle = 0
+
+
 		self.step1 = pygame.image.load("Sprites/Ant-1.png").convert_alpha()
 		self.step2 = pygame.image.load("Sprites/Ant-mid.png").convert_alpha()
 		self.step3 = pygame.image.load("Sprites/Ant-3.png").convert_alpha()
 		self.step4 = pygame.image.load("Sprites/Ant-mid.png").convert_alpha()
-		self.rect = self.image.get_rect()
 		self.step = [self.step1,self.step2,self.step3,self.step4]
-		self.perso_angle = 0
 		self.ticker = 0
 		self.rect.x = x
 		self.rect.y = y
@@ -33,8 +41,8 @@ class Hero(pygame.sprite.Sprite):
 		self.accx = 0
 		self.vitessey = 0
 		self.accy = 0
-		self.coordx = 1920/2
-		self.coordy = 1080/2
+		self.coordx = 300
+		self.coordy = 300
 
 		self.centerc = None
 		self.perso_angle = 0
@@ -43,18 +51,6 @@ class Hero(pygame.sprite.Sprite):
 		self.selectionned = False
 
 
-		self.selection = 0, 0
-		self.selectionned = False
-
-		self.xdest = 0
-		self.ydest = 0
-
-
-
-
-	def centerPos(self):
-		self.center = (self.rect.x + 56, self.rect.y + 75)
-		self.centerc = (self.coordx, self.coordy)
 
 	def update(self):
 
@@ -62,40 +58,37 @@ class Hero(pygame.sprite.Sprite):
 		if self.downKeyPressed:
 			self.accx = 0.1 * math.cos((-(self.perso_angle-360)+90) * (3.14/180))
 			self.accy = 0.1 * math.sin((-(self.perso_angle-360)+90) * (3.14/180))
-			mob_hit_list = pygame.sprite.spritecollide(self, self.mobs, False)
-			for mob in mob_hit_list:
-				if mob.hitpoint > 0:
-					self.rect.bottom = mob.rect.top
+
 			self.image = self.step[self.current_frame]
 					
 		if self.upKeyPressed:
 			self.accx = -0.1 * math.cos((-(self.perso_angle-360)+90) * (3.14/180))
 			self.accy = -0.1 * math.sin((-(self.perso_angle-360)+90) * (3.14/180))
-			mob_hit_list = pygame.sprite.spritecollide(self, self.mobs, False)
-			for mob in mob_hit_list:
-				if mob.hitpoint > 0:
-					self.rect.top = mob.rect.bottom
+
 			self.image = self.step[self.current_frame]
 
 		if self.leftKeyPressed:
-			self.perso_angle = (self.perso_angle + 1) % 360
-			self.perso_rotated_surf = pygame.transform.rotate(self.image, self.perso_angle)
-			self.rect = self.perso_rotated_surf.get_rect(center=self.centerc)
+			self.image = pygame.transform.rotate(self.original_image, self.angle)
+			self.angle += 5 % 360  # Value will reapeat after 359. This prevents angle to overflow.
+			x, y = self.rect.center  # Save its current center.
+			self.rect = self.image.get_rect()  # Replace old rect with new rect.
+			self.rect.center = (x, y)  # Put the new rect's center at old center.
 
 		if self.rightKeyPressed:
-			self.perso_angle = (self.perso_angle - 1) % 360
-			self.perso_rotated_surf = pygame.transform.rotate(self.image, self.perso_angle)
-			self.rect = self.perso_rotated_surf.get_rect(center=self.centerc)
+			self.image = pygame.transform.rotate(self.original_image, self.angle)
+			self.angle -= 5 % 360  # Value will reapeat after 359. This prevents angle to overflow.
+			x, y = self.rect.center  # Save its current center.
+			self.rect = self.image.get_rect()  # Replace old rect with new rect.
+			self.rect.center = (x, y)  # Put the new rect's center at old center.
 
-		xc, yc = self.center
-		xd, yd = self.selection
+		xc, yc = self.rect.center
 
 		if self.leftMousePressed:
 			xm, ym = self.selection
-			if xm >= (xc - 56):
-				if xm < (xc + 56):
-					if ym >= (yc - 75):
-						if ym < (yc + 75):
+			if xm >= (xc):
+				if xm < (xc + 150):
+					if ym >= (yc):
+						if ym < (yc + 150):
 							self.selectionned = True
 						else:
 							self.selectionned = False
@@ -107,52 +100,29 @@ class Hero(pygame.sprite.Sprite):
 				self.selectionned = False
 
 		if self.rightMousePressed:
+			xd, yd = self.selection
 			angled = (math.atan((xd-xc)/(yd-yc)))
-			self.xdest = xd
-			self.ydest = yd
 			if (xd-xc) >= 0:
-				if (yd-yc) == 0:
-					self.vitessey = 0
-					self.vitessex = -5
-					self.perso_angle = 270
-				if (yd-yc) > 0:
-					angledeg = ((angled*180) / 3.1415) + 90 #1
+				if (yd-yc) >= 0:
+					angledeg = ((angled*180) / 3.1415) + 180
 					if self.selectionned == True:
-						angle2 = (angledeg * 3.1415) / 180
-						self.perso_angle = angledeg
-						self.vitessex = -5 * math.cos(angle2)
-						self.vitessey = 5 * math.sin(angle2)
+						self.angle = angledeg
+						self.vitessex = 10 * math.cos(angled)
+						self.vitessey = 10 * math.sin(angled)
 				if (yd-yc) < 0:
-					angledeg = ((angled*180) / 3.1415) + 270 #2
+					angledeg = ((angled*180) / 3.1415) + 180
 					if self.selectionned == True:
-						angle2 = (angledeg * 3.1415) / 180
-						self.perso_angle = angledeg
-						self.vitessex = -5 * math.cos(angle2)
-						self.vitessey = 5 * math.sin(angle2)
+						self.angle = angledeg
+						self.vitessex = 10 * math.cos(angled)
+						self.vitessey = 10 * math.sin(angled)
 			if (xd-xc) < 0:
-				if (yd-yc) == 0:
-					self.vitessey = 0
-					self.vitessex = 5
-					self.perso_angle = 90
-				if (yd-yc) > 0:
-					angledeg = ((angled*180) / 3.1415) + 90 #3
-					if self.selectionned == True:
-						angle2 = (angledeg * 3.1415) / 180
-						self.perso_angle = angledeg
-						self.vitessex = -5 * math.cos(angle2)
-						self.vitessey = 5 * math.sin(angle2)
-				if (yd-yc) < 0:
-					angledeg = ((angled*180) / 3.1415) + 270 #4
-					if self.selectionned == True:
-						angle2 = (angledeg * 3.1415) / 180
-						self.perso_angle = angledeg
-						self.vitessex = -5 * math.cos(angle2)
-						self.vitessey = 5 * math.sin(angle2)
+				angledeg = ((angled*180) / 3.1415) + 180
+				if self.selectionned == True:
+					self.angle = angledeg
+					self.vitessex = 10 * math.cos(angled)
+					self.vitessey = 10 * math.sin(angled)
 
-		if xc >= (self.xdest - 10) and xc <= (self.xdest + 10) and yc >= (self.ydest - 10) and yc <= (self.ydest + 10):
-			self.vitessex = 0
-			self.vitessey = 0
-
+		print(self.angle)
 		self.ticker += 1
 		if self.ticker % 8 == 0:
 			self.current_frame = (self.current_frame + 1) % 4
@@ -172,19 +142,16 @@ class Hero(pygame.sprite.Sprite):
 class Room(object):
 	wall_list = None
 	def __init__(self):
-		self.mob_list = pygame.sprite.Group()
-		self.bullet = pygame.sprite.Group()
-		self.enemy_bullet = pygame.sprite.Group()
+		self.killed_mob = []
 
 class Room1(Room):
 	def __init__(self):
 		Room.__init__(self)
 		self.background = pygame.image.load("room/room1.png").convert_alpha()
-		mobs = []
 
 class GameMain():
 	done = False
-	
+
 	def __init__(self,width = 1920, height = 1080):
 		pygame.mixer.pre_init(44100, -16, 2, 2048)
 		pygame.init()
@@ -193,8 +160,7 @@ class GameMain():
 		self.width, self.height = width, height
 		pygame.display.set_caption("Space War !")
 		self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN)
-		self.hero = Hero(100,200,"UP",False,False,False,False,False,False,False,20, self)
-		self.bullet = pygame.sprite.Group()	
+		self.hero = Hero(100,200,"UP",False,False,False,False,False,False,False,20, self, pos=(150, 150))
 		self.all_sprite_list = pygame.sprite.Group()
 		self.all_sprite_list.add(self.hero)
 		self.rooms = [[Room1()]]
@@ -202,8 +168,6 @@ class GameMain():
 		self.current_x = 0
 		self.current_y = 0
 		self.current_room = self.rooms[self.current_y][self.current_x]
-		self.hero.mobs = self.rooms[self.current_y][self.current_x].mob_list
-
 
 		self.current_screen = "title"
 		
@@ -211,12 +175,8 @@ class GameMain():
 		while not self.done:
 			if self.current_screen == "game":
 					self.handle_events()
-					self.hero.centerPos()
 					self.draw()
 					self.all_sprite_list.update()
-					self.hero.mobs.update()
-					self.current_room.bullet.update()
-					self.current_room.enemy_bullet.update()
 					self.change_room()
 			elif self.current_screen == "title":
 				self.handle_events_title()
@@ -246,34 +206,25 @@ class GameMain():
 		self.screen.fill((255,255, 255))
 		background = self.current_room.background
 		self.screen.blit(background,(0,0))
-		self.all_sprite_list.draw(self.screen)
-		self.screen.blit(self.hero.perso_rotated_surf, self.hero.rect)
-		self.current_room.mob_list.draw(self.screen)
-		self.current_room.bullet.draw(self.screen)
-		self.current_room.enemy_bullet.draw(self.screen)			
+		self.all_sprite_list.draw(self.screen)			
 		pygame.display.flip()
 
 	def change_room(self):
 		if self.hero.coordx > 1921 :
 			self.hero.coordx = 0
 			self.current_room = self.rooms[self.current_y][self.current_x]
-			bullet_hit_list = self.current_room.bullet
 
 		elif self.hero.coordx < 0 :
 			self.hero.coordx = 1919
 			self.current_room = self.rooms[self.current_y][self.current_x]
-			bullet_hit_list = self.current_room.bullet
 
 		elif self.hero.coordy < 0 :
 			self.hero.coordy = 1079
 			self.current_room = self.rooms[self.current_y][self.current_x]
-			bullet_hit_list = self.current_room.bullet
-
 
 		elif self.hero.coordy > 1080 :
 			self.hero.coordy = 0
 			self.current_room = self.rooms[self.current_y][self.current_x]
-			bullet_hit_list = self.current_room.bullet
 
 	def handle_events(self):
 		events = pygame.event.get()
